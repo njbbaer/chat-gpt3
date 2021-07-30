c0e74453a253cb1513ece3e8b0bb9ea4e27f9138
Nate Baer <njbbaer@gmail.com>
2021-07-29 23:04:01 -0700

    Make retry button functional

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
+let previousText;
+
 $(document).ready(() => {
-  $('#prompt-input').val(
+  $('#api-key-input').val(sessionStorage.getItem('api-key'));
+  $('#text-input').val(
     'The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\n' +
     'Human: Hello, who are you?\n' +
     'AI: I am an AI created by OpenAI. How can I help you today?'
   );
 
-  $('#api-key').val(sessionStorage.getItem('api-key'));
-
   $('#submit-button').click(() => {
-    $('#prompt-input').val(
-      $('#prompt-input').val() + '\n' + $('#speech-input').val() + '\n'
-    );
-    $('#speech-input').val('Human: ');
-    sessionStorage.setItem('api-key', $('#api-key').val());
+    $('#text-input').val($('#text-input').val() + '\n' + $('#chat-input').val() + '\n')
+    $('#chat-input').val('Human: ');
     generateCompletion();
+  });
+
+  $('#retry-button').click(() => {
+    if (previousText) {
+      $('#text-input').val(previousText);
+      generateCompletion();
+    }
+  });
+
+  $('#api-key-input').change(() => {
+    sessionStorage.setItem('api-key', $('#api-key-input').val());
   });
 });
 
 const generateCompletion = async () => {
+  previousText = $('#text-input').val()
   const response = await fetch('https://api.openai.com/v1/engines/davinci/completions', {
     method: 'POST',
     body: JSON.stringify({
-      'prompt': $('#prompt-input').val() + 'AI:',
+      'prompt': $('#text-input').val() + 'AI:',
       'max_tokens': 128,
       'stop': '\n',
       'echo': true
     }),
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${$('#api-key').val()}`
+      'Authorization': `Bearer ${$('#api-key-input').val()}`
     }
   });
   const json = await response.json();
-  $('#prompt-input').val(json['choices'][0]['text']);
+  $('#text-input').val(json['choices'][0]['text']);
 }
