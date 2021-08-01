0f9872581c1d893e6e26d3b61f4ecf4cfe2bcfa8
Nate Baer <njbbaer@gmail.com>
2021-07-31 23:05:05 -0700

    Set up ESLint and resolve all warnings

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ let previousText;
 $(document).ready(() => {
   $('#api-key-input').val(localStorage.getItem('api-key'));
   $('#text-input').val(
-    'The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\n' +
-    'Human: Hello, who are you?\n' +
-    'AI: I am an AI created by OpenAI. How can I help you today?'
+    'The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\n'
+    + 'Human: Hello, who are you?\n'
+    + 'AI: I am an AI created by OpenAI. How can I help you today?',
   );
   updateCostEstimate();
 
   $('#submit-button').click(() => {
-    if ($('#chat-input').val()) {
-      updateTextInput($('#text-input').val() + '\nHuman: ' + $('#chat-input').val());
+    const textInput = $('#text-input').val();
+    const chatInput = $('#chat-input').val();
+    if (chatInput) {
+      updateTextInput(`${textInput}\nHuman: ${chatInput}`);
     }
     $('#chat-input').val('');
     generateCompletion();
@@ -30,36 +32,36 @@ $(document).ready(() => {
 });
 
 const generateCompletion = async () => {
-  previousText = $('#text-input').val();
+  const textInput = $('#text-input').val();
+  previousText = textInput;
   const response = await fetch('https://api.openai.com/v1/engines/davinci/completions', {
     method: 'POST',
     body: JSON.stringify({
-      'prompt': $('#text-input').val() + '\nAI:',
-      'max_tokens': 128,
-      'stop': '\n',
-      'echo': true
+      prompt: ` ${textInput} \nAI:`,
+      max_tokens: 128,
+      stop: '\n',
+      echo: true,
     }),
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${$('#api-key-input').val()}`
-    }
+      Authorization: `Bearer ${$('#api-key-input').val()}`,
+    },
   });
   const json = await response.json();
-  const text = json['choices'][0]['text'];
+  const { text } = json.choices[0];
   updateTextInput(text);
   updateCostEstimate(text.length);
-}
+};
 
 const updateTextInput = (text) => {
   $('#text-input').val(text);
   $('#text-input').scrollTop($('#text-input')[0].scrollHeight);
-}
-
+};
 
 const updateCostEstimate = (addChars = 0) => {
-  let totalChars = Number(sessionStorage.getItem('total-chars')) || 0
-  totalChars += addChars
+  let totalChars = Number(sessionStorage.getItem('total-chars')) || 0;
+  totalChars += addChars;
   sessionStorage.setItem('total-chars', totalChars);
-  const cost = (totalChars / 4 / 1024 * 0.06).toFixed(2);
-  $('#cost-estimate').text('💸 $' + cost);
-}
+  const cost = ((totalChars / 4 / 1024) * 0.06).toFixed(2);
+  $('#cost-estimate').text(`💸 $${cost}`);
+};
