ae429d515da8d82f38abafff3b6049794d8d7bac
Nate Baer <njbbaer@gmail.com>
2021-11-09 17:29:39 -0800

    Set custom names

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
 let previousText;
-let aiName;
 
 $(document).ready(() => {
-  $('#api-key-input').val(localStorage.getItem('api-key'));
+  $('#inputApiKey').val(localStorage.getItem('api-key'));
   updateCostEstimate();
 });
 
-$('#submit-button').click(() => {
-  const textInput = $('#text-input').val();
-  const chatInput = $('#chat-input').val();
-  const userName = $('#user-name').text();
-  if (chatInput) {
-    updateTextInput(`${textInput}\n${userName}${chatInput}`);
+$('#buttonSubmit').click(() => {
+  const textareaChat = $('#textareaChat').val();
+  const inputChat = $('#inputChat').val();
+  const inputUserName = $('#inputUserName').val();
+  if (inputChat) {
+    updateTextInput(`${textareaChat}\n${inputUserName}: ${inputChat}`);
   }
-  $('#chat-input').val('');
+  $('#inputChat').val('');
   generateCompletion();
 });
 
-$('#retry-button').click(() => {
+$('#buttonRetry').click(() => {
   if (previousText) {
-    $('#text-input').val(previousText);
+    $('#textareaChat').val(previousText);
     generateCompletion();
   }
 });
 
-$('#api-key-input').change(() => {
-  localStorage.setItem('api-key', $('#api-key-input').val());
+$('#inputApiKey').change(() => {
+  localStorage.setItem('api-key', $('#inputApiKey').val());
 });
 
-$("#template-dropdown a").click(function() {
-  $("#submit-button").prop("disabled", false);
-  $("#retry-button").prop("disabled", false);
-
+$("#dropdownTemplate a").click(function() {
   let selectedTemplate = $(this).text();
-  $("#selected-template").text(selectedTemplate);
-  if (selectedTemplate === 'AI Chat') {
-    aiName = 'AI:'
-    $('#user-name').text('Human: ');
-    $('#text-input').val(
+  $("#buttonTemplate").text(selectedTemplate);
+  if (selectedTemplate === 'Custom') {
+    $('#inputUserName').val('');
+    $('#inputAiName').val('');
+    $('#textareaChat').val('');
+  } else if (selectedTemplate === 'AI Chat') {
+    $('#inputUserName').val('Human');
+    $('#inputAiName').val('AI');
+    $('#textareaChat').val(
       'The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\n'
       + 'Human: Hello, who are you?\n'
       + 'AI: I am an AI created by OpenAI. How can I help you today?',
     );
   } else if (selectedTemplate === 'Fluttershy Therapist') {
-    aiName = 'Fluttershy:'
-    $('#user-name').text('Client: ');
-    $('#text-input').val(
+    $('#inputUserName').val('Client');
+    $('#inputAiName').val('Fluttershy');
+    $('#textareaChat').val(
       'Fluttershy is a character from My Little Pony Friendship is Magic who has opened a therapy practice. Fluttershy is kind, empathetic, and a good listener to her clients, whom she helps deal with their problems. The following is a transcript of a conversation between Flutttershy and a recent client.\n\n'
       + 'Fluttershy: Hello dear. How are you feeling today?'
     )
@@ -53,19 +53,20 @@ $("#template-dropdown a").click(function() {
 });
 
 const generateCompletion = async () => {
-  const textInput = $('#text-input').val();
-  previousText = textInput;
+  const textareaChat = $('#textareaChat').val();
+  const inputAiName = $('#inputAiName').val();
+  previousText = textareaChat;
   const response = await fetch('https://api.openai.com/v1/engines/davinci/completions', {
     method: 'POST',
     body: JSON.stringify({
-      prompt: `${textInput} \n${aiName}`,
+      prompt: `${textareaChat} \n${inputAiName}:`,
       max_tokens: 128,
       stop: '\n',
       echo: true,
     }),
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${$('#api-key-input').val()}`,
+      Authorization: `Bearer ${$('#inputApiKey').val()}`,
     },
   });
   const json = await response.json();
@@ -75,8 +76,8 @@ const generateCompletion = async () => {
 };
 
 const updateTextInput = (text) => {
-  $('#text-input').val(text);
-  $('#text-input').scrollTop($('#text-input')[0].scrollHeight);
+  $('#textareaChat').val(text);
+  $('#textareaChat').scrollTop($('#textareaChat')[0].scrollHeight);
 };
 
 const updateCostEstimate = (addChars = 0) => {
@@ -84,5 +85,5 @@ const updateCostEstimate = (addChars = 0) => {
   totalChars += addChars;
   sessionStorage.setItem('total-chars', totalChars);
   const cost = ((totalChars / 4 / 1024) * 0.06).toFixed(2);
-  $('#cost-estimate').text(`💸 $${cost}`);
+  $('#costEstimate').text(`💸 $${cost}`);
 };
