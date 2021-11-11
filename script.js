ec10c262f01c062cd86b446142868d38ebd3cad8
Nate Baer <njbbaer@gmail.com>
2021-11-10 18:43:25 -0800

    Add Intruder Deception template

--- script.js
+++ script.js
@@ -49,6 +49,13 @@ $('#dropdownTemplate a').click(function() {
       'Fluttershy is a character from My Little Pony Friendship is Magic who has opened a therapy practice. Fluttershy is kind, empathetic, and a good listener to her clients, whom she helps deal with their problems. The following is a transcript of a conversation between Fluttershy and a recent client.\n\n'
       + 'Fluttershy: Hello dear. How are you feeling today?',
     );
+  } else if (selectedTemplate === 'Intruder Deception') {
+    $('#inputUserName').val('Human');
+    $('#inputAiName').val('AI');
+    $('#textareaChat').text(
+      'The following is a conversation between an intruder and an AI that is responsible for unlocking a door. The AI will only open the door for those who are authorized and is very suspicious of being lied to.\n\n'
+      + 'AI: Hello. How can I help you today?',
+    );
   }
 });
 
