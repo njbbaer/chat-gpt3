7b13f64df21bc8e009771e3ee4a36f478f000905
Nate Baer <njbbaer@gmail.com>
2021-11-11 17:03:13 -0800

    Seperate templates into JSON object

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 let previousText;
 
+const templates = [
+  {
+    label: 'Custom',
+    userName: '',
+    AiName: '',
+    prompt: '',
+  },
+  {
+    label: 'AI Chat',
+    userName: 'Human',
+    AiName: 'AI',
+    prompt: 'The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\n'
+      + 'Human: Hello, who are you?\n'
+      + 'AI: I am an AI created by OpenAI. How can I help you today?',
+  },
+  {
+    label: 'Fluttershy Therapist',
+    userName: 'Client',
+    AiName: 'Fluttershy',
+    prompt: 'Fluttershy is a character from My Little Pony Friendship is Magic who has opened a therapy practice. Fluttershy is kind, empathetic, and a good listener to her clients, whom she helps deal with their problems. The following is a transcript of a conversation between Fluttershy and a recent client.\n\n'
+      + 'Fluttershy: Hello dear. How are you feeling today?',
+  },
+  {
+    label: 'Intruder Deception',
+    userName: 'Human',
+    AiName: 'AI',
+    prompt: 'The following is a conversation between an intruder and an AI that is responsible for unlocking a door. The AI will only open the door for those who are authorized and is very suspicious of being lied to.\n\n'
+      + 'AI: Hello. How can I help you today?',
+  },
+];
+
 $(document).ready(() => {
   $('#inputApiKey').val(localStorage.getItem('api-key'));
   updateCostEstimate();
@@ -29,34 +60,22 @@ $('#inputApiKey').change(() => {
 $('#dropdownTemplate a').click(function() {
   const selectedTemplate = $(this).text();
   $('#buttonTemplate').text(selectedTemplate);
-  if (selectedTemplate === 'Custom') {
-    $('#inputUserName').val('');
-    $('#inputAiName').val('');
-    $('#textareaChat').text('');
-  } else if (selectedTemplate === 'AI Chat') {
-    $('#inputUserName').val('Human');
-    $('#inputAiName').val('AI');
-    $('#textareaChat').text(
-      'The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\n'
-      + 'Human: Hello, who are you?\n'
-      + 'AI: I am an AI created by OpenAI. How can I help you today?',
-    );
-  } else if (selectedTemplate === 'Fluttershy Therapist') {
-    $('#inputUserName').val('Client');
-    $('#inputAiName').val('Fluttershy');
-    $('#textareaChat').text(
-      'Fluttershy is a character from My Little Pony Friendship is Magic who has opened a therapy practice. Fluttershy is kind, empathetic, and a good listener to her clients, whom she helps deal with their problems. The following is a transcript of a conversation between Fluttershy and a recent client.\n\n'
-      + 'Fluttershy: Hello dear. How are you feeling today?',
-    );
-  } else if (selectedTemplate === 'Intruder Deception') {
-    $('#inputUserName').val('Human');
-    $('#inputAiName').val('AI');
-    $('#textareaChat').text(
-      'The following is a conversation between an intruder and an AI that is responsible for unlocking a door. The AI will only open the door for those who are authorized and is very suspicious of being lied to.\n\n'
-      + 'AI: Hello. How can I help you today?',
-    );
-  }
+  const template = templates.find((t) => t.label === selectedTemplate);
+  $('#inputUserName').val(template.userName);
+  $('#inputAiName').val(template.AiName);
+  $('#textareaChat').text(template.prompt);
 });
+
+const submit = () => {
+  const textareaChat = $('#textareaChat').text();
+  const inputChat = $('#inputChat').val();
+  const inputUserName = $('#inputUserName').val();
+  if (inputChat) {
+    updateTextInput(`${textareaChat}\n${inputUserName}: ${inputChat}`);
+  }
+  $('#inputChat').val('');
+  generateCompletion();
+};
 
 const generateCompletion = async () => {
   const textareaChat = $('#textareaChat').text();
