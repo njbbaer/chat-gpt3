e37cb83a90ac6ee1f01247439bc3dd7c453e5582
Nate Baer <njbbaer@gmail.com>
2021-11-13 23:28:44 -0800

    Add mister rogers template

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const templates = [
   },
   {
     label: 'AI Chat',
-    userName: 'Human',
+    userName: 'You',
     aiName: 'AI',
     temperature: 0.8,
     prompt: 'The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\n'
@@ -18,7 +18,7 @@ const templates = [
   },
   {
     label: 'Fluttershy Therapist',
-    userName: 'Client',
+    userName: 'You',
     aiName: 'Fluttershy',
     temperature: 0.8,
     prompt: 'Fluttershy is a character from My Little Pony Friendship is Magic who has opened a therapy practice. Fluttershy is kind, empathetic, and a good listener to her clients, whom she helps deal with their problems. The following is a transcript of a conversation between Fluttershy and a recent client.\n\n'
@@ -26,11 +26,19 @@ const templates = [
   },
   {
     label: 'Intruder Deception',
-    userName: 'Human',
+    userName: 'You',
     aiName: 'AI',
     temperature: 0.95,
     prompt: 'The following is a conversation between an intruder and an AI that is responsible for unlocking a door. The AI will only open the door for those who are authorized and is very suspicious of being lied to.\n\n'
       + 'AI: Hello. How can I help you today?',
+  },
+  {
+    label: 'Mister Rogers',
+    userName: 'You',
+    aiName: 'Mister Rogers',
+    temperature: 0.8,
+    prompt: "Mister Rogers, was an American television host, author, producer, and Presbyterian minister. He was the creator, showrunner, and host of the preschool television series Mister Rogers' Neighborhood. Rogers attuned children and their developmental journeys to the most significant attributes of what it means to be a human: love, compassion and kindness for others. The following is a transcript of a conversation between Mister Rogers and an adult fan who has come to visit him in the neighborhood.\n\n"
+      + "Mister Rogers: Welcome. I'm so glad you've decided to come to the neighborhood, and I hope I can call you a friend.",
   },
 ];
 
